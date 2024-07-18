dee67c0795fcc76adf8ae74eb31f3f06e74551b4
miranas11 <anasmir24@gmail.com>
2024-07-19 00:11:30 +0400

    Following changes- -added the getRecommendedTags function

--- index.js
+++ index.js
@@ -1,78 +1,149 @@
 const cookieParser = require("cookie-parser");
 
-getWeek = (date) => {
-    const startOfYear = new Date(date.getFullYear(), 0, 1);
-    const pastDaysOfYear = (date - startOfYear) / 86400000;
+const express = require("express");
+const { getWeek, selectRandomItems } = require("./utils");
 
-    return Math.ceil((pastDaysOfYear + startOfYear.getDay() + 1) / 7);
+const app = express();
+
+app.use(cookieParser());
+
+const createUser = (req, res, userId) => {
+    try {
+        const cookieName = `${userId}_recommendations`;
+        if (!req.cookies || !req.cookies[cookieName]) {
+            res.cookie(
+                cookieName,
+                {
+                    lastUpdateWeek: getWeek(new Date()),
+                    tags: {},
+                },
+                { maxAge: 900000, httpOnly: true }
+            );
+            console.log(
+                `Empty cookie ${cookieName} created for user ${userId}.`
+            );
+        } else {
+            console.log(`User ${userId} already has the cookie ${cookieName}.`);
+        }
+        res.status(200).send("User cookie created or already exists.");
+    } catch (error) {
+        console.error("Error creating user cookie:", error);
+        res.status(500).send("Internal Server Error");
+    }
 };
 
-getWeek(new Date());
+const addTag = (req, res, userId, tagName) => {
+    try {
+        const cookieName = `${userId}_recommendations`;
+        let userActivity = req.cookies[cookieName];
 
-createUser = (req, res, userId) => {
-    const cookieName = `${userId}_recommendations`;
-    if (!req.cookies[cookieName]) {
-        res.cookies(cookieName, {
-            lastUpdateWeek: getWeek(new Date()),
-            tags: {},
+        if (!userActivity) {
+            userActivity = {
+                lastUpdateWeek: getWeek(new Date()),
+                tags: {},
+            };
+            console.log(
+                `Empty cookie ${cookieName} created for user ${userId}.`
+            );
+        } else {
+            userActivity = JSON.parse(userActivity);
+        }
+
+        if (!userActivity.tags[tagName]) {
+            userActivity.tags[tagName] = [[], [], []];
+            res.cookie(cookieName, JSON.stringify(userActivity), {
+                maxAge: 900000,
+                httpOnly: true,
+            });
+            console.log(
+                `Added tag ${tagName} with 3 epochs to cookie ${cookieName} for user ${userId}.`
+            );
+        } else {
+            console.log(
+                `Tag ${tagName} already exists in cookie ${cookieName} for user ${userId}.`
+            );
+        }
+        res.status(200).send(`Tag ${tagName} added or already exists.`);
+    } catch (error) {
+        console.error("Error adding tag:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+const addItemToTag = (req, res, userId, tagName, newItems) => {
+    try {
+        const cookieName = `${userId}_recommendations`;
+        const currentWeekNumber = getWeek(new Date());
+        let userActivity = req.cookies[cookieName];
+        if (!userActivity) {
+            userActivity = {
+                lastUpdateWeek: getWeek(new Date()),
+                tags: {},
+            };
+            userActivity.tags[tagName] = [[], [], []];
+        } else {
+            userActivity = JSON.parse(userActivity);
+        }
+
+        if (userActivity.lastUpdateWeek != currentWeekNumber) {
+            userActivity.tags[tagName].pop();
+            userActivity.tags[tagName].unshift([]);
+            userActivity.lastUpdateWeek = currentWeekNumber;
+        }
+
+        if (!Array.isArray(newItems)) {
+            newItems = [newItems];
+        }
+
+        newItems.forEach((item) => {
+            userActivity.tags[tagName][0].push(item);
         });
-        console.log(`Empty cookie ${cookieName} created for user ${userId}.`);
-    } else {
-        console.log(`User ${userId} already has the cookie ${cookieName}.`);
-    }
-};
 
-addTag = (req, res, tagName) => {
-    const cookieName = `${userId}_recommendations`;
-    let userActivity = req.cookies[cookieName]
-        ? JSON.parse(req.cookies[cookieName])
-        : {};
-    if (!userActivity) {
-        userActivity = {
-            lastUpdateWeek: getWeek(new Date()),
-            tags: {
-                tagName: [[], [], []],
-            },
-        };
-    }
-
-    if (!userActivity.tags.tagName) {
-        userActivity.tags[tagName] = [[], [], []];
-        res.cookies(cookieName, JSON.stringify(userActivity));
+        console.log(userActivity);
+        res.cookie(cookieName, JSON.stringify(userActivity), {
+            maxAge: 900000,
+            httpOnly: true,
+        });
         console.log(
-            `Added tag ${tagName} with 3 epochs to cookie ${cookieName} for user ${userId}.`
+            `Added items ${newItems} to epoch one of tag ${tagName} for user ${userId}.`
         );
-    } else {
-        console.log(
-            `Tag ${tagName} already exists in cookie ${cookieName} for user ${userId}.`
-        );
+        res.status(200).send(`Items added to epoch one of tag ${tagName}.`);
+    } catch (error) {
+        console.error("Error adding items to tag:", error);
+        res.status(500).send("Internal Server Error");
     }
 };
 
-addItemToTag = (req, res, tagName) => {
-    const cookieName = `${userId}_recommendations`;
-    const currentWeekNumber = getWeek(new Date());
-    let userActivity = req.cookies[cookieName]
-        ? JSON.parse(req.cookies[cookieName])
-        : {};
-    if (!userActivity) {
-        userActivity = {
-            lastUpdateWeek: getWeek(new Date()),
-            tags: {
-                tagName: [[], [], []],
-            },
-        };
-    }
+const getRecommendedTags = (req, res, userId, tagName) => {
+    try {
+        const cookieName = `${userId}_recommendations`;
+        let userActivity = req.cookies[cookieName];
+        if (!userActivity) {
+            console.log(`No cookie found with ${userId} userId`);
+            res.status(404).send(`No cookie found for user ${userId}`);
+            return;
+        }
+        userActivity = JSON.parse(userActivity);
+        if (!userActivity.tags[tagName]) {
+            console.log(`No tag found with ${tagName} name`);
+            res.status(404).send(`No tag found with name ${tagName}`);
+            return;
+        }
+        const epochs = userActivity.tags[tagName];
 
-    if (userActivity.lastUpdateWeek != currentWeekNumber) {
-        userActivity.tags[tagName].pop();
-        userActivity.tags[tagName].unshift([]);
-        userActivity.lastUpdateWeek = currentWeek;
+        const itemsFromFirstEpoch = selectRandomItems(epochs[0], 3);
+        const itemsFromSecondEpoch = selectRandomItems(epochs[1], 2);
+        const itemsFromThirdEpoch = selectRandomItems(epochs[2], 1);
+        const recommendedItems = [
+            ...itemsFromFirstEpoch,
+            ...itemsFromSecondEpoch,
+            ...itemsFromThirdEpoch,
+        ];
+        res.status(200).json(recommendedItems);
+    } catch (error) {
+        console.error("Error getting recommended tags:", error);
+        res.status(500).send("Internal Server Error");
     }
-    if (!userActivity) userActivity[tagName][0].push(newItem);
-
-    res.cookie(cookieName, JSON.stringify(userActivity));
-    console.log(
-        `Added item ${newItem} to epoch one of tag ${tagName} for user ${userId}.`
-    );
 };
+
+module.exports = { createUser, addTag, addItemToTag, getRecommendedTags };
---
+++ utils.js
@@ -0,0 +1,20 @@
+getWeek = (date) => {
+    const startOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear = (date - startOfYear) / 86400000;
+
+    return Math.ceil((pastDaysOfYear + startOfYear.getDay() + 1) / 7);
+};
+
+selectRandomItems = (array, numItems) => {
+    const selectedItems = new Set();
+    const maxItems = Math.min(numItems, array.length);
+
+    while (selectedItems.size < maxItems) {
+        const randomIndex = Math.floor(Math.random() * array.length);
+        selectedItems.add(array[randomIndex]);
+    }
+
+    return Array.from(selectedItems);
+};
+
+module.exports = { getWeek, selectRandomItems };
